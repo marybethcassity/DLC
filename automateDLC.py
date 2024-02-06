7c3e0ea1e7f09507e9f42f2db523c2580e9184b3
unknown <mary.beth.cassity@gmail.com>
2024-02-06 16:09:07 -0500

    Updated automateDLC.py

--- automateDLC.py
+++ automateDLC.py
@@ -1,29 +1,22 @@
 import deeplabcut
-import sys
-from config import *
+import argparse
 
 def train_network(config_path):
-    
     deeplabcut.train_network(config_path, displayiters=1000, saveiters=20000, maxiters=100000)
 
 def evaluate_network(config_path):
-    
-    deeplabcut.evaluate_network(config_path, plotting=True)  
+    deeplabcut.evaluate_network(config_path, plotting=True)
 
-def analyze_videos(config_path):
-   
-    deeplabcut.analyze_videos(config_path, video_path, save_as_csv=True)
+def analyze_videos(config_path, video_path):
+    deeplabcut.analyze_videos(config_path, [video_path], save_as_csv=True)
 
 def create_labeled_video(config_path, video_path):
-    
-    deeplabcut.create_labeled_video(config_path, video_path)
+    deeplabcut.create_labeled_video(config_path, [video_path])
 
 def extract_outliers(config_path, video_path):
-    
-    deeplabcut.extract_outlier_frames(config_path, video_path, outlieralgorithm='uncertain')
+    deeplabcut.extract_outlier_frames(config_path, [video_path], outlieralgorithm='uncertain')
 
 def full_pipeline(config_path, video_path):
-
     train_network(config_path)
     evaluate_network(config_path)
     analyze_videos(config_path, video_path)
@@ -31,12 +24,25 @@ def full_pipeline(config_path, video_path):
     extract_outliers(config_path, video_path)
 
 def main():
-    if len(sys.argv) < 2 or sys.argv[1].lower() != 'run_all':
-        print("Usage: python script.py run_all")
-        sys.exit(1)
+    parser = argparse.ArgumentParser(description="DeepLabCut helper script.")
+    parser.add_argument('function', type=str, choices=['train_network', 'evaluate_network', 'analyze_videos', 'create_labeled_video', 'extract_outliers', 'full_pipeline'], help="Function to run")
+    parser.add_argument('config_path', type=str, help="Path to the config file")
+    parser.add_argument('video_path', type=str, nargs='?', default='', help="Path to the video file")
 
-    full_pipeline()
+    args = parser.parse_args()
 
+    if args.function == 'train_network':
+        train_network(args.config_path)
+    elif args.function == 'evaluate_network':
+        evaluate_network(args.config_path)
+    elif args.function in ['analyze_videos', 'create_labeled_video', 'extract_outliers']:
+        if args.video_path == '':
+            raise ValueError("video_path is required for {}".format(args.function))
+        globals()[args.function](args.config_path, args.video_path)
+    elif args.function == 'full_pipeline':
+        if args.video_path == '':
+            raise ValueError("video_path is required for full_pipeline")
+        full_pipeline(args.config_path, args.video_path)
 
 if __name__ == "__main__":
-    main(config_path, video_path)
+    main()
