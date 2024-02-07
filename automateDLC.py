25c421498a6c1e5e4570d1c5a9339345e50e39c7
unknown <mary.beth.cassity@gmail.com>
2024-02-07 12:04:00 -0500

    Updated automateDLC.py

--- automateDLC.py
+++ automateDLC.py
@@ -1,5 +1,7 @@
 import deeplabcut
 import argparse
+from config import *
+
 
 def train_network(config_path):
     deeplabcut.train_network(config_path, displayiters=1000, saveiters=20000, maxiters=100000)
@@ -7,46 +9,39 @@ def train_network(config_path):
 def evaluate_network(config_path):
     deeplabcut.evaluate_network(config_path, plotting=True)
 
-def analyze_videos(config_path, video_path):
-    deeplabcut.analyze_videos(config_path, [video_path], save_as_csv=True)
+def analyze_videos(config_path, video_paths):
+    deeplabcut.analyze_videos(config_path, video_paths, save_as_csv=True)
 
-def plot_trajectories(config_path, video_path):
-    deeplabcut.plot_trajectories(config_path, [video_path])    
+def plot_trajectories(config_path, video_paths):
+    deeplabcut.plot_trajectories(config_path, video_paths)    
 
-def create_labeled_video(config_path, video_path):
-    deeplabcut.create_labeled_video(config_path, [video_path])
+def create_labeled_video(config_path, video_paths):
+    deeplabcut.create_labeled_video(config_path, video_paths)
 
-def extract_outliers(config_path, video_path):
-    deeplabcut.extract_outlier_frames(config_path, [video_path], outlieralgorithm='jump')
+def extract_outliers(config_path, video_paths):
+    deeplabcut.extract_outlier_frames(config_path, video_paths, outlieralgorithm='jump', automatic = True)
 
-def full_pipeline(config_path, video_path):
-    train_network(config_path)
+def full_pipeline(config_path, video_paths):
+    #train_network(config_path)
     evaluate_network(config_path)
-    analyze_videos(config_path, video_path)
-    plot_trajectories(config_path, video_path)
-    create_labeled_video(config_path, video_path)
-    extract_outliers(config_path, video_path)
+    analyze_videos(config_path, video_paths)
+    plot_trajectories(config_path, video_paths)
+    create_labeled_video(config_path, video_paths)
+    extract_outliers(config_path, video_paths)
 
 def main():
     parser = argparse.ArgumentParser(description="DeepLabCut helper script.")
-    parser.add_argument('function', type=str, choices=['train_network', 'evaluate_network', 'analyze_videos', 'create_labeled_video', 'extract_outliers', 'full_pipeline'], help="Function to run")
-    parser.add_argument('config_path', type=str, help="Path to the config file")
-    parser.add_argument('video_path', type=str, nargs='?', default='', help="Path to the video file")
+    parser.add_argument('function', type=str, choices=['train_network', 'evaluate_network', 'analyze_videos', 'plot_trajectories', 'create_labeled_video', 'extract_outliers', 'full_pipeline'], help="Function to run")
 
     args = parser.parse_args()
 
     if args.function == 'train_network':
-        train_network(args.config_path)
+        train_network(config_path)
     elif args.function == 'evaluate_network':
-        evaluate_network(args.config_path)
-    elif args.function in ['analyze_videos', 'plot_trajectories', 'create_labeled_video', 'extract_outliers']:
-        if args.video_path == '':
-            raise ValueError("video_path is required for {}".format(args.function))
-        globals()[args.function](args.config_path, args.video_path)
-    elif args.function == 'full_pipeline':
-        if args.video_path == '':
-            raise ValueError("video_path is required for full_pipeline")
-        full_pipeline(args.config_path, args.video_path)
+        evaluate_network(config_path)
+    elif args.function in ['analyze_videos', 'plot_trajectories', 'create_labeled_video', 'extract_outliers', 'full_pipeline']:
+        globals()[args.function](config_path, video_paths)
+
 
 if __name__ == "__main__":
     main()
